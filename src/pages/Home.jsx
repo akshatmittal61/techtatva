a0669ec5c936f50e7aeeb44853702d0cca6dde8c
akshatmittal61 <akshatmittal2506@gmail.com>
2021-12-20 12:51:27 +0530

    Completed scroll effect in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import favicon from "../images/favicon.png";
@@ -7,6 +7,15 @@ import iiitu from "../images/iiitu.gif";
 import aavesh from "../images/aavesh.png";
 
 const Home = () => {
+	const maxY = Math.round(window.innerHeight / 100) * 50;
+	const [scroll_img, setScroll_img] = useState(
+		window.scrollY < maxY ? maxY - window.scrollY : maxY - maxY
+	);
+	window.addEventListener("scroll", () => {
+		setScroll_img(
+			window.scrollY < maxY ? maxY - window.scrollY : maxY - maxY
+		);
+	});
 	AOS.init();
 	return (
 		<section className="home">
@@ -21,28 +30,29 @@ const Home = () => {
 						className="home-hero-image__img"
 						data-aos="zoom-in"
 						data-aos-duration="1500"
+						style={{
+							maxWidth: `${scroll_img * (100 / maxY)}%`,
+							maxHeight: `${scroll_img * (100 / maxY)}%`,
+						}}
 					/>
 				</div>
-				<div className="home-hero-content" data-aos="fade-up">
+				<div
+					className="home-hero-content"
+					data-aos="fade-up"
+					style={{
+						position: scroll_img > 0 ? "relative" : "fixed",
+					}}
+				>
 					<span style={{ fontSize: "3.5rem", fontWeight: "700" }}>
 						TECHतत्त्व:
 					</span>
-					<span>Innovation Inspired</span>
+					<span>Innovation Inspired: {scroll_img}</span>
 				</div>
 			</div>
 			<div
 				className="home-about"
 				style={{ backgroundImage: `url(${bg})` }}
-			>
-				<div className="home-about-logos">
-					<div className="home-about-logo">
-						<img className="home-about-logo__img" src={iiitu} />
-					</div>
-					<div className="home-about-logo">
-						<img className="home-about-logo__img" src={aavesh} />
-					</div>
-				</div>
-			</div>
+			></div>
 		</section>
 	);
 };
